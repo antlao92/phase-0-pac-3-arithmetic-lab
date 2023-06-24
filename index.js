32ff9552ab3409add9fc2b5cedf3ddbcfb0e3cbb
antlao92 <anthonykimlao@gmail.com>
2023-06-24 05:07:18 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+return a+=b;
+}
+
+function subtract(a,b){
+return a-=b;
+}
+function multiply(a,b){
+return a*=b;
+}
+function divide(a,b){
+return a/=b;   
+}
+
+function increment(n){
+return n+=1
+}
+
+function decrement(n){
+    return n-=1
+    }
+
+function makeInt(String){
+    return parseInt(String,10)
+    }
+
+    function preserveDecimal(n){
+        return parseFloat(n,10)
+        }
+
